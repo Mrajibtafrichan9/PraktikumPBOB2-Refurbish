d353b43b7a713b71cf9d7e7f34a4c028a30ab797
Xodiac <mrajibtafrichan9@gmail.com>
2026-03-08 20:55:48 +0700

    InLab03 : update 3

--- Praktikum 03 Relasi/In Lab/src/Mahasiswa2.java
+++ Praktikum 03 Relasi/In Lab/src/Mahasiswa2.java
@@ -14,11 +14,37 @@ public class Mahasiswa2 {
     /* Konstruktor */
     //
     Mahasiswa2(){
+        this.NamaMhs2 = "XXX";
+        this.NIM = "xxxxxx";
+        this.Prodi = "-";
+        this.DosenWali = null;
+        this.Kendaraan = null;
+        this.ListMatKul = new ArrayList<>();
+    }
+
+    public Mahasiswa2(String NamaMhs2, String NIM, String Prodi, Dosen DosenWali, Kendaraan Kendaraan){
+        this.NamaMhs2 = NamaMhs2;
+        this.NIM = NIM;
+        this.Prodi = Prodi;
+        this.DosenWali = DosenWali;
+        this.Kendaraan = Kendaraan;
         this.ListMatKul = new ArrayList<>();
     }
 
     void addMatkul(MataKuliah newMataKuliah){
         ListMatKul.add(newMataKuliah);
+    }
+
+    void getJumlahSKS(){
+        int totalSKS = 0;
+        for(int i = 0; i < ListMatKul.size(); i++){
+            totalSKS += ListMatKul.get(i).getSKS();
+        }
+        System.out.println("Total SKS : " + totalSKS);
+    }
+
+    void getJumlahMatKul(){
+        System.out.println("Jumlah Mata Kuliah : " + ListMatKul.size());
     }
 
     void printMhs2(){
--- Praktikum 03 Relasi/In Lab/src/MainMhs2.java
+++ Praktikum 03 Relasi/In Lab/src/MainMhs2.java
@@ -1,5 +1,12 @@
 public class MainMhs2 {
     public static void main(String [] args){
-        
+        Mahasiswa2 M1 = new Mahasiswa2("Suharnomo", "H5786NY", "Teknik Informatika", new Dosen("Dr. Budi Santoso", "NIP123456", "Teknik Informatika"), new Kendaraan("H5786NY", "Sepeda Motor"));
+        MataKuliah MK1 = new MataKuliah("IF101", "Pemrograman Dasar", 3);
+        MataKuliah MK2 = new MataKuliah("IF102", "Struktur Data", 4);       
+        M1.addMatkul(MK1);
+        M1.addMatkul(MK2);  
+        M1.printMhs2();
+        M1.getJumlahSKS();
+        M1.getJumlahMatKul();
     }
 }
